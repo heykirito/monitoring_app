adce36b03ec61a0bf5053bbe9154321e907168f4
Laksh <raftaarmalhotra@gmail.com>
2025-10-04 13:25:40 +0530

    app done, running locally

---
+++ app.py
@@ -0,0 +1,21 @@
+from token import INDENT
+import psutil
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    cpu_usage = psutil.cpu_percent()
+    mem_usage = psutil.virtual_memory().percent
+    msg = None
+    if cpu_usage > 80 or mem_usage > 80:
+        msg = "High CPU or memory usgae detected"
+    return render_template(
+        "index.html", cpu_usage=cpu_usage, mem_usage=mem_usage, message=msg
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
