2c2229c17bd9146ce253a0a7552d21942049ea7b
Laksh <raftaarmalhotra@gmail.com>
2025-10-09 12:29:33 +0530

    ECR file added using boto3 module

---
+++ ecr.py
@@ -0,0 +1,9 @@
+import boto3
+
+ecr_client = boto3.client("ecr")
+
+repo_name = "cloud-monitoring-app"
+response = ecr_client.create_repository(repositoryName=repo_name)
+
+repo_uri = response["repository"]["repositoryUri"]
+print(repo_uri)
